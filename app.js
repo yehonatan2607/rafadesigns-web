9720908bd4eab61601dba46d0e033f2ed30e2622
yehonatan natan <yehonatanrefael592@gmail.com>
2022-06-18 19:33:04 +0300

    added navbar

--- app.js
+++ app.js
@@ -4,5 +4,14 @@ var path = require("path");
 var app = express();
 const PORT = process.env.PORT || 8080
 
-app.listen(PORT);
-console.log("server listening in port ", PORT);
+app.get('/api/customers', (req, res) =>{
+    const cusromers = [
+        {id: 1, firstName:"yehonatan", lastName: "natan"},
+        {id: 2, firstName:"ricky", lastName: "natan"},
+        {id: 3, firstName:"guy", lastName: "natan"},
+    ];
+    res.json(cusromers);
+});
+
+app.listen(PORT, () => console.log("server listening in port ", PORT));
+
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Navbar.css'
+function Navbar() {
+  return (
+    <nav>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Resume</a></li>
+            <li><a href="">Portfolio</a></li>
+            <li><a href="">Services</a></li>
+        </ul>
+    </nav>
+
+  )
+}
+
+export default Navbar
